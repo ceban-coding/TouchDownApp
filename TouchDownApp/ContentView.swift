d97965d111c519718ed32efa97d31c999169fcb2
Ion Ceban <ceban.ion1992@gmail.com>
2021-02-04 19:28:50 -0600

    Initial Commit

---
+++ TouchDownApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TouchDownApp
+//
+//  Created by Ion Ceban on 2/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TouchDownApp/TouchDownAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownAppApp.swift
+//  TouchDownApp
+//
+//  Created by Ion Ceban on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
